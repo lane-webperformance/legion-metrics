35956aaec740f91cb218ed163dd39c46bfe146b5
Christopher Lane Hinson <lane@webperformance.com>
2017-05-18 17:10:55 -0400

    Add a couple more tags.

--- src/tags/index.js
+++ src/tags/index.js
@@ -2,16 +2,20 @@
 
 module.exports.generic = require('./generic');
 
+module.exports.everything = module.exports.generic('everything','everything');
+
 module.exports.outcome = module.exports.generic('outcome');
 module.exports.outcome.success = module.exports.outcome('success');
 module.exports.outcome.failure = module.exports.outcome('failure');
 module.exports.outcome.timeout = module.exports.outcome('timeout');
 
 module.exports.protocol = module.exports.generic('protocol');
+
 module.exports.service = module.exports.generic('service');
+module.exports.serviceCall = module.exports.generic('service-call');
 
 module.exports.api = module.exports.generic('api');
-module.exports.apicall = module.exports.generic('apicall');
+module.exports.apiCall = module.exports.generic('api-call');
 
 module.exports.testcase = module.exports.generic('testcase');
 module.exports.testcaseCompletion = module.exports.generic('testcaseCompletion');
